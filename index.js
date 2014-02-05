860b2cbc30cf225ea80448898458f924616ad997
Ben West <bewest@gmail.com>
2014-02-05 10:35:37 -0800

    create tool to validate all types

--- index.js
+++ index.js
@@ -5,21 +5,36 @@ var jsonschema = require('jsonschema')
   , util = require('util')
   ;
 
-
 var diabetes = require('./schemas/diabetes/');
 var schema = require('./schemas/diabetes.json');
 
-
-var schemas = [
-    diabetes.smbg
-  , schema
-  ];
+function getSchemaFor (o) {
+  var out = o;
+  switch (o) {
+    case 'bolus':
+      out = diabetes.bolusNormal;
+      break;
+    case 'smbg':
+      out = diabetes.smbg;
+      break;
+    case 'carbs':
+      out = diabetes.carbs;
+      break;
+    case null:
+    case '':
+    case 'diabetes':
+    default:
+      out = schema;
+      break;
+  }
+  return out;
+}
 
 function create (opts) {
   opts = opts || { };
-  var config = {sync: true};
   var validator = new jsonschema.Validator( );
-  validator.addSchema(schema, '/');
+  var master = getSchemaFor(opts.schema);
+  validator.addSchema(master, '/');
   function importMissing ( ) {
     var missing = validator.unresolvedRefs.shift( );
     if (!missing) { return; };
@@ -31,24 +46,31 @@ function create (opts) {
     }
   }
   importMissing( );
-  return validator;
+
+
+  function validate (data) {
+    return validator.validate(data, master);
+  }
+  return validate;
 }
 
 
 module.exports = create;
+module.exports.stream = stream;
+module.exports.getSchemaFor = getSchemaFor;
 
-function stream (V) {
+function stream (validate) {
   var out = es.through(writer);
   var S;
   function writer (data) {
     var self = this;
-    self.queue(V.validate(data, schema));
+    self.queue(validate(data, schema));
   }
   return out;
 }
 
 if (!module.parent) {
-  var datum = create( );
+  var validator = create( );
   var incoming = process.argv[2];
   if (incoming == '-' || !incoming) {
     incoming = process.stdin;
@@ -56,10 +78,10 @@ if (!module.parent) {
   } else {
     incoming = fs.createReadStream(incoming);
   }
-  es.pipeline(incoming, es.parse( ), stream(datum), es.writeArray(done));
+  es.pipeline(incoming, es.parse( ), stream(validator), es.writeArray(done));
   function done (err, results) {
     var report = results.pop( );
-    var status = util.format("OK: %d valid records", report.instance.length);
+    var status = util.format("OK: %d valid records", report.instance.length || results.length + 1);
     if (report.errors.length > 0) {
       status = util.format("Errors:", report.errors);
     }
