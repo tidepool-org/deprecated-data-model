3c3b3cd90eb285d7a2259d8a19c0dc3932372193
Ben West <bewest@gmail.com>
2014-02-04 16:07:42 -0800

    stub out some schemas and examples

---
+++ schemas/diabetes/index.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    smbg: require('./smbg.json')
+  , carbs: require('./carbs.json')
+  , cbg: require('./cbg.json')
+  , bolusNormal: require('./bolus/normal.json')
+  , bolusDual: require('./bolus/dual.json')
+  , basal: require('./basal/index.json')
+};
+
