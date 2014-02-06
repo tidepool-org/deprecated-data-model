376e613d8ca6741f6b1975bb4d9c5d666585209e
Ben West <bewest@gmail.com>
2014-02-05 19:51:57 -0800

    test that schemas don't validate things they shouldn't

--- tests/test_one_is_one.js
+++ tests/test_one_is_one.js
@@ -1,4 +1,5 @@
 var should = require('should');
+var util = require('util');
 
 describe("each sample has a schema", function ( ) {
   var types = [
@@ -12,17 +13,47 @@ describe("each sample has a schema", function ( ) {
     , 'cbg.json'
     , 'smbg.json'
   ];
+  function pathFor (t) {
+    var p = '../schemas/diabetes/' + t.replace('-', '/');
+    return p;
+  }
+  function instanceFor (t) {
+    var i = require('../examples/one/' + t);
+    return i;
+  }
   var V = require('../');
   types.forEach(function (type) {
-    var p = '../schemas/diabetes/' + type.replace('-', '/');
+    var p = pathFor(type);
     var S = require(p);
     var validate = V({schema: S});
-    var i = require('../examples/one/' + type);
+    var i = instanceFor(type);
     it(type + ' should validate the sample', function ( ) {
       var report = validate(i);
       report.should.be.ok;
       report.errors.should.be.empty;
 
+    });
+
+    types.forEach(function (other) {
+      if (other !== type) {
+        var otherSchema = require(pathFor(other));
+        var otherValidate = V({schema: otherSchema});
+        var otherInstance = instanceFor(other);
+        var name = util.format("schema %s should cause instance %s to fail", type, other);
+        it(name, function ( ) {
+          var report = validate(otherInstance);
+          console.log(report);
+          report.should.be.ok;
+          report.errors.should.not.be.empty;
+        });
+        name = util.format("schema %s should cause instance %s to fail", other, type);
+        it(name, function ( ) {
+          var report = otherValidate(i);
+          console.log(report);
+          report.should.be.ok;
+          report.errors.should.not.be.empty;
+        });
+      }
     });
   });
 });
