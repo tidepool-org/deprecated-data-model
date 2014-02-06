c19507e775f847d817152f1ad9d03356fc188667
Ben West <bewest@gmail.com>
2014-02-06 14:34:24 -0800

    atually enable travis publishing

--- publish-gh-pages.sh
+++ publish-gh-pages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "push build results to gh-pages"
-cd web/
-(git push -q origin gh-pages) 2>&1 > /dev/null || echo "oops"
+(cd web/
+ git push -q origin gh-pages) 2>&1 > /dev/null || echo "oops"
 
--- travis-setup.sh
+++ travis-setup.sh
@@ -4,4 +4,6 @@
 echo "Setting up travis"
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
+GIT_URL=${GH_TOKEN}@github.com:${TRAVIS_REPO_SLUG}.git
+git config remote.origin.url ${GIT_URL}
 
