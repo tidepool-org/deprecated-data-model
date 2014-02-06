96d82c500815bc61f644f83e59c29d7bc376c2bd
Ben West <bewest@gmail.com>
2014-02-06 14:48:24 -0800

    say yes to are you sure you want to continue connecting

--- publish-gh-pages.sh
+++ publish-gh-pages.sh
@@ -2,5 +2,5 @@
 
 echo "push build results to gh-pages"
 (cd web/
- git push -q origin gh-pages) 2>&1 > /dev/null || echo "oops"
+ yes | git push -q origin gh-pages) 2>&1 > /dev/null || echo "oops"
 
