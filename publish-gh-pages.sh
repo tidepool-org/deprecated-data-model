329aa3d576528895c6c6a208d8573f92c6635715
Ben West <bewest@gmail.com>
2014-02-06 16:18:19 -0800

    add release tool

--- publish-gh-pages.sh
+++ publish-gh-pages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "$TRAVIS_BRANCH from $TRAVIS_REPO_SLUG"
 if [[ -n "$TRAVIS" && "$TRAVIS_REPO_SLUG" != "tidepool-org/data-model" && "$TRAVIS_BRANCH" != "master" ]]; then
 echo $TRAVIS_BRANCH should not update gh-pages, only master
 exit 0;
@@ -7,5 +8,6 @@ exit 0;
 fi
 echo "push build results to gh-pages"
 (cd web/
- yes | git push -q origin gh-pages 2>&1 > /dev/null) || echo "oops"
+  git status
+  yes | git push -q origin gh-pages 2>&1 > /dev/null) || echo "oops"
 
