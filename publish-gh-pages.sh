686f7b030953943e1d9c5713264848b1d872bdc0
Ben West <bewest@gmail.com>
2014-02-06 16:09:14 -0800

    only master branch from origin should update gh-pages

--- publish-gh-pages.sh
+++ publish-gh-pages.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -n "$TRAVIS" && "$TRAVIS_REPO_SLUG" != "tidepool-org/data-model" && "$TRAVIS_BRANCH" != "master" ]]; then
+echo $TRAVIS_BRANCH should not update gh-pages, only master
+exit 0;
+
+fi
 echo "push build results to gh-pages"
 (cd web/
  yes | git push -q origin gh-pages 2>&1 > /dev/null) || echo "oops"
